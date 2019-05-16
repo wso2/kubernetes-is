f640352db8359d4e9cfbdc8625cef313b571f813
Chiranga Alwis <chirangaalwis@gmail.com>
2019-05-16 18:51:04 +0530

    Improve deployment management scripts for each pattern to support users with and without subscriptions

--- is-with-analytics/scripts/deploy.sh
+++ is-with-analytics/scripts/deploy.sh
@@ -19,117 +19,146 @@
 set -e
 
 ECHO=`which echo`
-KUBECTL=`which kubectl`
+GREP=`which grep`
+KUBERNETES_CLIENT=`which kubectl`
+SED=`which sed`
+TEST=`which test`
 
 # methods
 function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
 }
 
-function usage () {
-    echoBold "This script automates the installation of WSO2 Identity Server with Analytics Kubernetes resources\n"
-    echoBold "Allowed arguments:\n"
-    echoBold "-h | --help"
-    echoBold "--wu | --wso2-username\t\tYour WSO2 username"
-    echoBold "--wp | --wso2-password\t\tYour WSO2 password"
-    echoBold "--cap | --cluster-admin-password\tKubernetes cluster admin password\n\n"
-}
+read -p "Do you have a WSO2 Subscription? (Y/N)" -n 1 -r
+${ECHO}
 
-WSO2_SUBSCRIPTION_USERNAME=''
-WSO2_SUBSCRIPTION_PASSWORD=''
-ADMIN_PASSWORD=''
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+    read -p "Enter Your WSO2 Username: " WSO2_SUBSCRIPTION_USERNAME
+    ${ECHO}
+    read -s -p "Enter Your WSO2 Password: " WSO2_SUBSCRIPTION_PASSWORD
+    ${ECHO}
 
-# capture named arguments
-while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
+    HAS_SUBSCRIPTION=0
 
-    case ${PARAM} in
-        -h | --help)
-            usage
+    if ! ${GREP} -q "imagePullSecrets" \
+    ../is/identity-server-deployment.yaml \
+    ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
+    ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
+
+        if ! ${SED} -i.bak -e 's|wso2/|docker.wso2.com/|' \
+        ../is/identity-server-deployment.yaml \
+        ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
+        ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
+            echoBold "Could not configure to use the Docker image available at WSO2 Private Docker Registry (docker.wso2.com)"
             exit 1
-            ;;
-        --wu | --wso2-username)
-            WSO2_SUBSCRIPTION_USERNAME=${VALUE}
-            ;;
-        --wp | --wso2-password)
-            WSO2_SUBSCRIPTION_PASSWORD=${VALUE}
-            ;;
-        --cap | --cluster-admin-password)
-            ADMIN_PASSWORD=${VALUE}
-            ;;
-        *)
-            echoBold "ERROR: unknown parameter \"${PARAM}\""
-            usage
+        fi
+
+        if ! ${SED} -i.bak -e '/serviceAccount/a \      imagePullSecrets:' \
+        ../is/identity-server-deployment.yaml \
+        ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
+        ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
+            echoBold "Could not configure Kubernetes Docker image pull secret: Failed to create \"imagePullSecrets:\" attribute"
             exit 1
-            ;;
-    esac
-    shift
-done
+        fi
+
+
+        if ! ${SED} -i.bak -e '/imagePullSecrets/a \      - name: wso2creds' \
+        ../is/identity-server-deployment.yaml \
+        ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
+        ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
+            echoBold "Could not configure Kubernetes Docker image pull secret: Failed to create secret name"
+            exit 1
+        fi
+    fi
+elif [[ ${REPLY} =~ ^[Nn]$ || -z "${REPLY}" ]]; then
+     HAS_SUBSCRIPTION=1
+
+     if ! ${SED} -i.bak -e '/imagePullSecrets:/d' -e '/- name: wso2creds/d' \
+     ../is/identity-server-deployment.yaml \
+     ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
+     ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
+         echoBold "Failed to remove the Kubernetes Docker image pull secret"
+         exit 1
+     fi
+
+    if ! ${SED} -i.bak -e 's|docker.wso2.com|wso2|' \
+     ../is/identity-server-deployment.yaml \
+     ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
+     ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
+        echoBold "Could not configure to use the WSO2 Docker image available at DockerHub"
+        exit 1
+    fi
+else
+    echoBold "You have entered an invalid option."
+    exit 1
+fi
+
+# remove backed up files
+${TEST} -f ../is/*.bak && rm ../is/*.bak
+${TEST} -f ../is-analytics-dashboard/*.bak && rm ../is-analytics-dashboard/*.bak
+${TEST} -f ../is-analytics-worker/*.bak && rm ../is-analytics-worker/*.bak
 
 # create a new Kubernetes Namespace
-${KUBECTL} create namespace wso2
+${KUBERNETES_CLIENT} create namespace wso2
 
 # create a new service account in 'wso2' Kubernetes Namespace
-${KUBECTL} create serviceaccount wso2svc-account -n wso2
+${KUBERNETES_CLIENT} create serviceaccount wso2svc-account -n wso2
 
 # switch the context to new 'wso2' namespace
-${KUBECTL} config set-context $(${KUBECTL} config current-context) --namespace=wso2
+${KUBERNETES_CLIENT} config set-context $(${KUBERNETES_CLIENT} config current-context) --namespace=wso2
 
-# create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
-${KUBECTL} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
-
+if [[ ${HAS_SUBSCRIPTION} -eq 0 ]]; then
+    # create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
+    ${KUBERNETES_CLIENT} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
+fi
 # create Kubernetes Role and Role Binding necessary for the Kubernetes API requests made from Kubernetes membership scheme
-${KUBECTL} create --username=admin --password=${ADMIN_PASSWORD} -f ../../rbac/rbac.yaml
+${KUBERNETES_CLIENT} create -f ../../rbac/rbac.yaml
 
 # create Kubernetes ConfigMaps
 echoBold 'Creating ConfigMaps...'
-${KUBECTL} create configmap identity-server-conf --from-file=../confs/is/conf/
-${KUBECTL} create configmap identity-server-conf-axis2 --from-file=../confs/is/conf/axis2/
-${KUBECTL} create configmap identity-server-conf-datasources --from-file=../confs/is/conf/datasources/
-${KUBECTL} create configmap identity-server-conf-identity --from-file=../confs/is/conf/identity/
-${KUBECTL} create configmap identity-server-conf-event-publishers --from-file=../confs/is/deployment/server/eventpublishers/
-${KUBECTL} create configmap identity-server-conf-tomcat --from-file=../confs/is/conf/tomcat/
-
-${KUBECTL} create configmap is-analytics-worker-conf --from-file=../confs/is-analytics-worker/conf/worker
-
-${KUBECTL} create configmap is-analytics-dashboard-conf --from-file=../confs/is-analytics-dashboard/conf/dashboard
-
-${KUBECTL} create configmap mysql-dbscripts --from-file=../extras/confs/rdbms/mysql/dbscripts/
+${KUBERNETES_CLIENT} create configmap identity-server-conf --from-file=../confs/is/conf/
+${KUBERNETES_CLIENT} create configmap identity-server-conf-axis2 --from-file=../confs/is/conf/axis2/
+${KUBERNETES_CLIENT} create configmap identity-server-conf-datasources --from-file=../confs/is/conf/datasources/
+${KUBERNETES_CLIENT} create configmap identity-server-conf-identity --from-file=../confs/is/conf/identity/
+${KUBERNETES_CLIENT} create configmap identity-server-conf-event-publishers --from-file=../confs/is/deployment/server/eventpublishers/
+${KUBERNETES_CLIENT} create configmap identity-server-conf-tomcat --from-file=../confs/is/conf/tomcat/
+${KUBERNETES_CLIENT} create configmap is-analytics-worker-conf --from-file=../confs/is-analytics-worker/conf/worker
+${KUBERNETES_CLIENT} create configmap is-analytics-dashboard-conf --from-file=../confs/is-analytics-dashboard/conf/dashboard
+${KUBERNETES_CLIENT} create configmap mysql-dbscripts --from-file=../extras/confs/rdbms/mysql/dbscripts/
 
 echoBold 'Deploying the Kubernetes Services...'
-${KUBECTL} create -f ../extras/rdbms/mysql/mysql-service.yaml
-${KUBECTL} create -f ../is/identity-server-service.yaml
-${KUBECTL} create -f ../is-analytics-worker/identity-server-analytics-worker-service.yaml
-${KUBECTL} create -f ../is-analytics-dashboard/identity-server-analytics-dashboard-service.yaml
+${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-service.yaml
+${KUBERNETES_CLIENT} create -f ../is/identity-server-service.yaml
+${KUBERNETES_CLIENT} create -f ../is-analytics-worker/identity-server-analytics-worker-service.yaml
+${KUBERNETES_CLIENT} create -f ../is-analytics-dashboard/identity-server-analytics-dashboard-service.yaml
 sleep 10s
 
 # persistent storage
 echoBold 'Creating persistent volume and volume claim...'
-${KUBECTL} create -f ../is/identity-server-volume-claims.yaml
-${KUBECTL} create -f ../extras/rdbms/mysql/mysql-persistent-volume-claim.yaml
-${KUBECTL} create -f ../volumes/persistent-volumes.yaml
-${KUBECTL} create -f ../extras/rdbms/volumes/persistent-volumes.yaml
+${KUBERNETES_CLIENT} create -f ../is/identity-server-volume-claims.yaml
+${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-persistent-volume-claim.yaml
+${KUBERNETES_CLIENT} create -f ../volumes/persistent-volumes.yaml
+${KUBERNETES_CLIENT} create -f ../extras/rdbms/volumes/persistent-volumes.yaml
 sleep 10s
 
 # MySQL
 echoBold 'Deploying WSO2 Identity Server and Identity Server Analytics Databases using MySQL...'
-${KUBECTL} create -f ../extras/rdbms/mysql/mysql-deployment.yaml
+${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-deployment.yaml
 sleep 60s
 
 # Identity Server and Analytics
 echoBold 'Deploying WSO2 Identity Server and Analytics...'
-${KUBECTL} create -f ../is/identity-server-deployment.yaml
-${KUBECTL} create -f ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml
+${KUBERNETES_CLIENT} create -f ../is/identity-server-deployment.yaml
+${KUBERNETES_CLIENT} create -f ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml
 sleep 30s
-${KUBECTL} create -f ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml
+${KUBERNETES_CLIENT} create -f ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml
 sleep 30s
 
-#echoBold 'Deploying Ingresses...'
-${KUBECTL} create -f ../ingresses/identity-server-ingress.yaml
-${KUBECTL} create -f ../ingresses/identity-server-dashboard-ingress.yaml
+echoBold 'Deploying Ingresses...'
+${KUBERNETES_CLIENT} create -f ../ingresses/identity-server-ingress.yaml
+${KUBERNETES_CLIENT} create -f ../ingresses/identity-server-dashboard-ingress.yaml
 sleep 30s
 
 echoBold 'Finished'
 echo 'To access the WSO2 Identity Server management console, try https://wso2is/carbon in your browser.'
-echo 'To access the WSO2 Identity Server Analytics management console, try https://wso2is-dashboard/portal in your browser.'
+echo 'To access the WSO2 Identity Server Analytics management console, try https://wso2is-analytics-dashboard/portal in your browser.'
--- is/scripts/deploy.sh
+++ is/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ------------------------------------------------------------------------
-# Copyright 2018 WSO2, Inc. (http://wso2.com)
+# Copyright 2017 WSO2, Inc. (http://wso2.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,104 +19,117 @@
 set -e
 
 ECHO=`which echo`
-KUBECTL=`which kubectl`
+GREP=`which grep`
+KUBERNETES_CLIENT=`which kubectl`
+SED=`which sed`
+TEST=`which test`
 
 # methods
 function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
 }
 
-function usage () {
-    echoBold "This script automates the installation of WSO2 Identity Server Kubernetes resources\n"
-    echoBold "Allowed arguments:\n"
-    echoBold "-h | --help"
-    echoBold "--wu | --wso2-username\t\tYour WSO2 username"
-    echoBold "--wp | --wso2-password\t\tYour WSO2 password"
-    echoBold "--cap | --cluster-admin-password\tKubernetes cluster admin password\n\n"
-}
+read -p "Do you have a WSO2 Subscription? (Y/N)" -n 1 -r
+${ECHO}
 
-WSO2_SUBSCRIPTION_USERNAME=''
-WSO2_SUBSCRIPTION_PASSWORD=''
-ADMIN_PASSWORD=''
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+    read -p "Enter Your WSO2 Username: " WSO2_SUBSCRIPTION_USERNAME
+    ${ECHO}
+    read -s -p "Enter Your WSO2 Password: " WSO2_SUBSCRIPTION_PASSWORD
+    ${ECHO}
 
-# capture named arguments
-while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
+    HAS_SUBSCRIPTION=0
 
-    case ${PARAM} in
-        -h | --help)
-            usage
+    if ! ${GREP} -q "imagePullSecrets" ../identity-server-deployment.yaml; then
+
+        if ! ${SED} -i.bak -e 's|wso2/|docker.wso2.com/|' ../identity-server-deployment.yaml; then
+            echoBold "Could not configure to use the Docker image available at WSO2 Private Docker Registry (docker.wso2.com)"
             exit 1
-            ;;
-        --wu | --wso2-username)
-            WSO2_SUBSCRIPTION_USERNAME=${VALUE}
-            ;;
-        --wp | --wso2-password)
-            WSO2_SUBSCRIPTION_PASSWORD=${VALUE}
-            ;;
-        --cap | --cluster-admin-password)
-            ADMIN_PASSWORD=${VALUE}
-            ;;
-        *)
-            echoBold "ERROR: unknown parameter \"${PARAM}\""
-            usage
+        fi
+
+        if ! ${SED} -i.bak -e '/serviceAccount/a \      imagePullSecrets:' ../identity-server-deployment.yaml; then
+            echoBold "Could not configure Kubernetes Docker image pull secret: Failed to create \"imagePullSecrets:\" attribute"
             exit 1
-            ;;
-    esac
-    shift
-done
+        fi
 
-# create a new Kubernetes Namespace
-${KUBECTL} create namespace wso2
 
-# create a new service account in 'wso2' Kubernetes Namespace
-${KUBECTL} create serviceaccount wso2svc-account -n wso2
+        if ! ${SED} -i.bak -e '/imagePullSecrets/a \      - name: wso2creds' ../identity-server-deployment.yaml; then
+            echoBold "Could not configure Kubernetes Docker image pull secret: Failed to create secret name"
+            exit 1
+        fi
+    fi
+elif [[ ${REPLY} =~ ^[Nn]$ || -z "${REPLY}" ]]; then
+     HAS_SUBSCRIPTION=1
 
-# switch the context to new 'wso2' namespace
-${KUBECTL} config set-context $(${KUBECTL} config current-context) --namespace=wso2
+     if ! ${SED} -i.bak -e '/imagePullSecrets:/d' -e '/- name: wso2creds/d' ../identity-server-deployment.yaml; then
+         echoBold "Failed to remove the Kubernetes Docker image pull secret"
+         exit 1
+     fi
 
-# create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
-${KUBECTL} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
+    if ! ${SED} -i.bak -e 's|docker.wso2.com|wso2|' ../identity-server-deployment.yaml; then
+        echoBold "Could not configure to use the WSO2 Docker image available at DockerHub"
+        exit 1
+    fi
+else
+    echoBold "You have entered an invalid option."
+    exit 1
+fi
 
-# create Kubernetes Role and Role Binding necessary for the Kubernetes API requests made from Kubernetes membership scheme
-${KUBECTL} create --username=admin --password=${ADMIN_PASSWORD} -f ../../rbac/rbac.yaml
+# remove backed up files
+${TEST} -f ../*.bak && rm ../*.bak
 
-# create Kubernetes ConfigMaps
-echoBold 'Creating Kubernetes ConfigMaps...'
-${KUBECTL} create configmap identity-server-conf --from-file=../confs/
-${KUBECTL} create configmap identity-server-conf-axis2 --from-file=../confs/axis2/
-${KUBECTL} create configmap identity-server-conf-datasources --from-file=../confs/datasources/
-${KUBECTL} create configmap identity-server-conf-identity --from-file=../confs/identity/
-${KUBECTL} create configmap mysql-dbscripts --from-file=../extras/confs/rdbms/mysql/dbscripts/
-
-echoBold 'Deploying the Kubernetes Services...'
-${KUBECTL} create -f ../extras/rdbms/mysql/mysql-service.yaml
-${KUBECTL} create -f ../identity-server-service.yaml
-sleep 10s
-
-# deploying WSO2 product databases using MySQL RDBMS
-echoBold 'Deploying WSO2 Identity Server Databases using MySQL...'
-${KUBECTL} create -f ../extras/rdbms/mysql/mysql-deployment.yaml
-sleep 10s
-
-# persistent storage
-echoBold 'Creating persistent volumes and volume claims...'
-${KUBECTL} create -f ../identity-server-volume-claims.yaml
-${KUBECTL} create -f ../volumes/persistent-volumes.yaml
-${KUBECTL} create -f ../extras/rdbms/mysql/mysql-persistent-volume-claim.yaml
-${KUBECTL} create -f ../extras/rdbms/volumes/persistent-volumes.yaml
-sleep 10s
-
-# WSO2 Identity Server
-echoBold 'Deploying WSO2 Identity Server...'
-${KUBECTL} create -f ../identity-server-deployment.yaml
-sleep 30s
-
-# create Kubernetes Ingress resources
-echoBold 'Deploying Kubernetes Ingresses...'
-${KUBECTL} create -f ../ingresses/identity-server-ingress.yaml
-sleep 30s
-
-echoBold 'Finished'
-echoBold 'To access the WSO2 Identity Server management console, try https://wso2is/carbon in your browser.'
+## create a new Kubernetes Namespace
+#${KUBERNETES_CLIENT} create namespace wso2
+#
+## create a new service account in 'wso2' Kubernetes Namespace
+#${KUBERNETES_CLIENT} create serviceaccount wso2svc-account -n wso2
+#
+## switch the context to new 'wso2' namespace
+#${KUBERNETES_CLIENT} config set-context $(${KUBERNETES_CLIENT} config current-context) --namespace=wso2
+#
+#if [[ ${HAS_SUBSCRIPTION} -eq 0 ]]; then
+#    # create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
+#    ${KUBERNETES_CLIENT} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
+#fi
+#
+## create Kubernetes Role and Role Binding necessary for the Kubernetes API requests made from Kubernetes membership scheme
+#${KUBERNETES_CLIENT} create -f ../../rbac/rbac.yaml
+#
+## create Kubernetes ConfigMaps
+#echoBold 'Creating Kubernetes ConfigMaps...'
+#${KUBERNETES_CLIENT} create configmap identity-server-conf --from-file=../confs/
+#${KUBERNETES_CLIENT} create configmap identity-server-conf-axis2 --from-file=../confs/axis2/
+#${KUBERNETES_CLIENT} create configmap identity-server-conf-datasources --from-file=../confs/datasources/
+#${KUBERNETES_CLIENT} create configmap identity-server-conf-identity --from-file=../confs/identity/
+#${KUBERNETES_CLIENT} create configmap mysql-dbscripts --from-file=../extras/confs/rdbms/mysql/dbscripts/
+#
+#echoBold 'Deploying the Kubernetes Services...'
+#${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-service.yaml
+#${KUBERNETES_CLIENT} create -f ../identity-server-service.yaml
+#sleep 10s
+#
+## deploying WSO2 product databases using MySQL RDBMS
+#echoBold 'Deploying WSO2 Identity Server Databases using MySQL...'
+#${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-deployment.yaml
+#sleep 10s
+#
+## persistent storage
+#echoBold 'Creating persistent volumes and volume claims...'
+#${KUBERNETES_CLIENT} create -f ../identity-server-volume-claims.yaml
+#${KUBERNETES_CLIENT} create -f ../volumes/persistent-volumes.yaml
+#${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-persistent-volume-claim.yaml
+#${KUBERNETES_CLIENT} create -f ../extras/rdbms/volumes/persistent-volumes.yaml
+#sleep 10s
+#
+## WSO2 Identity Server
+#echoBold 'Deploying WSO2 Identity Server...'
+#${KUBERNETES_CLIENT} create -f ../identity-server-deployment.yaml
+#sleep 30s
+#
+## create Kubernetes Ingress resources
+#echoBold 'Deploying Kubernetes Ingresses...'
+#${KUBERNETES_CLIENT} create -f ../ingresses/identity-server-ingress.yaml
+#sleep 30s
+#
+#echoBold 'Finished'
+#echoBold 'To access the WSO2 Identity Server management console, try https://wso2is/carbon in your browser.'
