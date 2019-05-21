fcfcab5aa23aa08361c3ba5c97edfa9836854726
Chiranga Alwis <chirangaalwis@gmail.com>
2019-05-21 14:06:10 +0530

    Integrate changes between WSO2 Identity Server and Analytics RC-1 and RC-3

--- is-with-analytics/scripts/deploy.sh
+++ is-with-analytics/scripts/deploy.sh
@@ -19,84 +19,12 @@
 set -e
 
 ECHO=`which echo`
-GREP=`which grep`
 KUBERNETES_CLIENT=`which kubectl`
-SED=`which sed`
-TEST=`which test`
 
 # methods
 function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
 }
-
-read -p "Do you have a WSO2 Subscription? (Y/N)" -n 1 -r
-${ECHO}
-
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-    read -p "Enter Your WSO2 Username: " WSO2_SUBSCRIPTION_USERNAME
-    ${ECHO}
-    read -s -p "Enter Your WSO2 Password: " WSO2_SUBSCRIPTION_PASSWORD
-    ${ECHO}
-
-    HAS_SUBSCRIPTION=0
-
-    if ! ${GREP} -q "imagePullSecrets" \
-    ../is/identity-server-deployment.yaml \
-    ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
-    ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
-
-        if ! ${SED} -i.bak -e 's|wso2/|docker.wso2.com/|' \
-        ../is/identity-server-deployment.yaml \
-        ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
-        ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
-            echoBold "Could not configure to use the Docker image available at WSO2 Private Docker Registry (docker.wso2.com)"
-            exit 1
-        fi
-
-        if ! ${SED} -i.bak -e '/serviceAccount/a \      imagePullSecrets:' \
-        ../is/identity-server-deployment.yaml \
-        ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
-        ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
-            echoBold "Could not configure Kubernetes Docker image pull secret: Failed to create \"imagePullSecrets:\" attribute"
-            exit 1
-        fi
-
-
-        if ! ${SED} -i.bak -e '/imagePullSecrets/a \      - name: wso2creds' \
-        ../is/identity-server-deployment.yaml \
-        ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
-        ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
-            echoBold "Could not configure Kubernetes Docker image pull secret: Failed to create secret name"
-            exit 1
-        fi
-    fi
-elif [[ ${REPLY} =~ ^[Nn]$ || -z "${REPLY}" ]]; then
-     HAS_SUBSCRIPTION=1
-
-     if ! ${SED} -i.bak -e '/imagePullSecrets:/d' -e '/- name: wso2creds/d' \
-     ../is/identity-server-deployment.yaml \
-     ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
-     ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
-         echoBold "Failed to remove the Kubernetes Docker image pull secret"
-         exit 1
-     fi
-
-    if ! ${SED} -i.bak -e 's|docker.wso2.com|wso2|' \
-     ../is/identity-server-deployment.yaml \
-     ../is-analytics-dashboard/identity-server-analytics-dashboard-deployment.yaml \
-     ../is-analytics-worker/identity-server-analytics-worker-deployment.yaml; then
-        echoBold "Could not configure to use the WSO2 Docker image available at DockerHub"
-        exit 1
-    fi
-else
-    echoBold "You have entered an invalid option."
-    exit 1
-fi
-
-# remove backed up files
-${TEST} -f ../is/*.bak && rm ../is/*.bak
-${TEST} -f ../is-analytics-dashboard/*.bak && rm ../is-analytics-dashboard/*.bak
-${TEST} -f ../is-analytics-worker/*.bak && rm ../is-analytics-worker/*.bak
 
 # create a new Kubernetes Namespace
 ${KUBERNETES_CLIENT} create namespace wso2
@@ -107,10 +35,6 @@ ${KUBERNETES_CLIENT} create serviceaccount wso2svc-account -n wso2
 # switch the context to new 'wso2' namespace
 ${KUBERNETES_CLIENT} config set-context $(${KUBERNETES_CLIENT} config current-context) --namespace=wso2
 
-if [[ ${HAS_SUBSCRIPTION} -eq 0 ]]; then
-    # create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
-    ${KUBERNETES_CLIENT} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
-fi
 # create Kubernetes Role and Role Binding necessary for the Kubernetes API requests made from Kubernetes membership scheme
 ${KUBERNETES_CLIENT} create -f ../../rbac/rbac.yaml
 
