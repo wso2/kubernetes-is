2af6c584641e155dd8b04d9ac939021a15bd7cbd
NishikaDeSilva <desilvanishika73@gmail.com>
2019-05-23 12:04:24 +0530

    Add simplified setup

---
+++ simple/single-script/create/create.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+IS_OPEN_SOURCE=false
+
+if $IS_OPEN_SOURCE; then
+    SCRIPT="../wso2is-ga.sh"
+else
+    SCRIPT="../wso2is-latest.sh"
+fi
+
+cat > $SCRIPT << "EOF"
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------------
+
+set -e
+EOF
+
+cat >> $SCRIPT << "EOF"
+# bash variables
+k8s_obj_file="deployment.yaml"; NODE_IP=''; str_sec=""
+EOF
+
+if $IS_OPEN_SOURCE; then
+  echo 'IMG_DEST="wso2"' >> $SCRIPT
+else
+  echo 'IMG_DEST="docker.wso2.com"' >> $SCRIPT
+fi
+
+cat >> $SCRIPT << "EOF"
+
+# wso2 subscription variables
+WUMUsername=''; WUMPassword=''
+
+: ${namespace:="wso2"}
+: ${randomPort:="False"}; : ${NP_1:=30443};
+
+# testgrid directory
+OUTPUT_DIR=$4; INPUT_DIR=$2; TG_PROP="$INPUT_DIR/infrastructure.properties"
+EOF
+
+echo "function create_yaml(){" >> $SCRIPT
+
+echo 'cat > $k8s_obj_file << "EOF"' >> $SCRIPT; echo 'EOF' >> $SCRIPT
+echo 'if [ "$namespace" == "wso2" ]; then' >> $SCRIPT
+
+echo 'cat >> $k8s_obj_file << "EOF"' >> $SCRIPT
+cat ../../pre-req/wso2is-namespace.yaml >> $SCRIPT
+echo -e "EOF\nfi">> $SCRIPT
+
+echo 'cat >> $k8s_obj_file << "EOF"' >> $SCRIPT
+cat ../../pre-req/wso2is-serviceaccount.yaml >> $SCRIPT
+if ! $IS_OPEN_SOURCE ; then
+  cat ../../pre-req/wso2is-secret.yaml >> $SCRIPT
+fi
+cat ../../confs/is-confs.yaml >> $SCRIPT
+cat ../../confs/is-conf-ds.yaml >> $SCRIPT
+cat ../../confs/mysql-conf-db.yaml >> $SCRIPT
+cat ../../mysql/wso2is-mysql-service.yaml >> $SCRIPT
+cat ../../is/wso2is-service.yaml >> $SCRIPT
+cat ../../mysql/wso2is-mysql-deployment.yaml >> $SCRIPT
+cat ../../is/wso2is-deployment.yaml >> $SCRIPT
+echo 'EOF' >> $SCRIPT
+echo "}" >> $SCRIPT
+
+if $IS_OPEN_SOURCE; then
+    cat funcs4opensource >> $SCRIPT
+else
+    cat funcs >> $SCRIPT
+fi
+
+cat >> $SCRIPT << "EOF"
+arg=$1
+if [[ -z $arg ]]
+then
+    echoBold "Expected parameter is missing\n"
+    usage
+else
+  case $arg in
+    -d|--deploy)
+      deploy
+      ;;
+    -u|--undeploy)
+      undeploy
+      ;;
+    -h|--help)
+      usage
+      ;;
+    *)
+      echoBold "Invalid parameter\n"
+      usage
+      ;;
+  esac
+fi
+EOF
