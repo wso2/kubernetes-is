3929f60d20e62f5d6f2f56691c8de91bc71c6d1d
NishikaDeSilva <desilvanishika73@gmail.com>
2019-10-11 20:57:59 +0530

    Remove overriding configurations

--- simple/deployment-scripts/wso2is-ga.sh
+++ simple/deployment-scripts/wso2is-ga.sh
@@ -114,36 +114,6 @@ data:
 ---
 
 apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: identity-server-conf-entrypoint
-  namespace : wso2
-data:
-  docker-entrypoint.sh: |
-    #!/bin/sh
-
-    set -e
-
-    # volume mounts
-    config_volume=${WORKING_DIRECTORY}/wso2-config-volume
-    artifact_volume=${WORKING_DIRECTORY}/wso2-artifact-volume
-
-    # check if the WSO2 non-root user home exists
-    test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
-
-    # check if the WSO2 product home exists
-    test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
-
-    # copy any configuration changes mounted to config_volume
-    test -d ${config_volume} && [ "$(ls -A ${config_volume})" ] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
-    # copy any artifact changes mounted to artifact_volume
-    test -d ${artifact_volume} && [ "$(ls -A ${artifact_volume})" ] && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
-
-    # start WSO2 Carbon server
-    sh ${WSO2_SERVER_HOME}/bin/wso2server.sh "$@"
----
-
-apiVersion: v1
 data:
   init.sql: |
     DROP DATABASE IF EXISTS WSO2IS_SHARED_DB;
@@ -2149,7 +2119,6 @@ spec:
           initialDelaySeconds: 250
           periodSeconds: 10
         imagePullPolicy: Always
-        command: ["/home/wso2carbon/entrypoint.sh"]
         resources:
           requests:
             memory: "2Gi"
@@ -2179,9 +2148,6 @@ spec:
         - name: identity-server-conf
           mountPath: /home/wso2carbon/wso2-config-volume/repository/conf/deployment.toml
           subPath: deployment.toml
-        - name: identity-server-conf-entrypoint
-          mountPath: /home/wso2carbon/entrypoint.sh
-          subPath: docker-entrypoint.sh
       serviceAccountName: "wso2svc-account"
       imagePullSecrets:
         - name: wso2is-deployment-creds
@@ -2189,10 +2155,6 @@ spec:
       - name: identity-server-conf
         configMap:
           name: identity-server-conf
-      - name: identity-server-conf-entrypoint
-        configMap:
-          name: identity-server-conf-entrypoint
-          defaultMode: 0407
 ---
 EOF
 }
